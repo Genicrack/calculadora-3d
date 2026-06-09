bc89df2914e6a3cc04896a1209b340d94ff473a6
Genicrack <genarobarbera75@gmail.com>
2026-06-09 10:42:53 -0300

    Refactor cloud config to export firebase configuration

--- cloud-config.js
+++ cloud-config.js
@@ -1,11 +1,9 @@
-window.CALC3D_CLOUD = {
-  enabled: false,
-  firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  }
+export const firebaseConfig = {
+  apiKey: "AIzaSyBbgpYErnHdiUltyYsFvj-qGFguFiE7M94",
+  authDomain: "calculadora-3d-2e897.firebaseapp.com",
+  projectId: "calculadora-3d-2e897",
+  storageBucket: "calculadora-3d-2e897.firebasestorage.app",
+  messagingSenderId: "334853691413",
+  appId: "1:334853691413:web:71dcfcc97206e37e32d61c",
+  measurementId: "G-FGBJ7F5Q3E"
 };
